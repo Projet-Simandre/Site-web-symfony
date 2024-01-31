b67186d50e9b503eba5820f3e9086a1e89e05c8d
Alex-hexa <alexandre.mulard@ecole-hexagone.com>
2024-01-31 15:17:24 +0100

    ajout des différentes pages

--- src/Controller/CardController.php
+++ src/Controller/CardController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class CardController extends AbstractController
 {
     #[Route('/carte', name: 'card')]
     public function index(): Response
